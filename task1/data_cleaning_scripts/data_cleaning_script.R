c67ec087b7785c9b5fa8234e7b8227f3fbca1efc
Sid-Rodrigues <sidrodrigues1@gmail.com>
2020-07-24 11:40:05 +0100

    project submit

--- task1/data_cleaning_scripts/data_cleaning_script.R
+++ task1/data_cleaning_scripts/data_cleaning_script.R
@@ -6,18 +6,18 @@ library(dplyr)
 
 #Load the data
 decathlon <- read_rds("raw_data/decathlon.rds")
-decathlon
+
 
 #1.1.1 Cleaning data
-class(decathlon)
-head(decathlon)
-names(decathlon)
-dim(decathlon)
-glimpse(decathlon)
+#class(decathlon)
+#head(decathlon)
+#names(decathlon)
+#dim(decathlon)
+#glimpse(decathlon)
 
 #This data has row names!
 
-rownames(decathlon)
+#rownames(decathlon)
 
 #To get rid of rownames, use function rownames_to_column() from the package 'tibble'
 #Create new data frame with new variable 'name' as its first column
@@ -30,8 +30,8 @@ new_decathlon <- decathlon %>%
 
 #Check column names
 
-colnames(new_decathlon)
-glimpse(new_decathlon)
+#colnames(new_decathlon)
+#glimpse(new_decathlon)
 
 #This data isn't tidy. Names column has mix values; upped and lower case. 
 #Using mutate/ str_to_lower() to convert values to lower case
@@ -49,7 +49,8 @@ decathlon_clean <- decathlon_clean %>%
                names_to = "events",
                values_to = "result")
 
-decathlon_clean
+#decathlon_clean
+
 #Checks
 #names(decathlon_clean)
 #decathlon_clean
--- task4/data_cleaning_scripts/data_cleaning_script.R
+++ task4/data_cleaning_scripts/data_cleaning_script.R
@@ -1,22 +1,17 @@
 
+#1.4 Task 4 - Halloween Candy Data
+
 library(readxl)
 library(readr)
 library(dplyr)
 library(here)
 library(tidyverse)
+library(stringr)
 
 
-# Read in xlsx file
+# Read in xlsx file no. 1
 
 sheet1 <- read_xlsx(here("raw_data/boing-boing-candy-2015.xlsx"))
-sheet1
-
-
-# Inspect the data i.e. dimensions, variable types, variable names, etc.?
-
-dim(sheet1)
-colnames(sheet1)
-glimpse(sheet1)
 
 
 # Tidy data - long format
@@ -27,49 +22,27 @@ sheet1_long <- sheet1 %>%
                values_to = "rating")
 
 
-#check dimensions, column names
-
-dim(sheet1_long)
-colnames(sheet1_long)
-
-
 #Omitting columns that are not required
 
 sheet1_long <- sheet1_long %>%
   select(-c(4:29))
 
 
-#check dimensions, column names
-
-colnames(sheet1_long)
-dim(sheet1_long)
-
-
-#Change column names
+#Adding new column names
 
 colnames(sheet1_long) <- c("timestamp", "age", "going_out", "candy", "rating")
-colnames(sheet1_long)
 
 
-#Add column 'year'
+#Add new column 'year'
 
 sheet1_long <- sheet1_long %>%
   add_column(year = 2015)
-head(sheet1_long)
 
-#x x x x x x x x x x x x x x x x x x x x x x x x x 
 
-# Read in xlsx file
+
+# Read in xlsx file no. 2
 
 sheet2 <- read_xlsx(here("raw_data/boing-boing-candy-2016.xlsx"))
-sheet2
-
-
-# Inspect the data i.e. dimensions, variable types, variable names, etc.?
-
-dim(sheet2)
-colnames(sheet2)
-glimpse(sheet2)
 
 
 # Tidy data - long format
@@ -80,49 +53,26 @@ sheet2_long <- sheet2 %>%
                values_to = "rating")
 
 
-##check dimensions, column names
-dim(sheet2_long)
-colnames(sheet2_long)
-
-
 #Omitting columns that are not required
 
 sheet2_long <- sheet2_long %>%
   select(-c(6:22))
 
 
-#check dimensions, column names
-
-colnames(sheet2_long)
-dim(sheet2_long)
-
-
-#Change column names
+#Adding new column names
 
 colnames(sheet2_long) <- c("timestamp", "going_out", "gender", "age", "country", "candy", "rating")
-colnames(sheet2_long)
 
 
 #Add column 'year'
 
 sheet2_long <- sheet2_long %>%
   add_column(year = 2016)
-head(sheet2_long)
 
 
-#x x x x x x x x x x x x x x x x x x x x x x x x x x x x x 
-
-# Read in xlsx file
+# Read in xlsx file no. 3
 
 sheet3 <- read_xlsx(here("raw_data/boing-boing-candy-2017.xlsx"))
-sheet3
-
-
-# Inspect the data i.e. dimensions, variable types, variable names, etc.?
-
-dim(sheet3)
-colnames(sheet3)
-glimpse(sheet3)
 
 
 # Tidy data - long format
@@ -131,7 +81,6 @@ sheet3_long <- sheet3 %>%
   pivot_longer(cols = starts_with("Q6"),  #converting rows to columns 
                names_to = "candy",
                values_to = "rating")
-colnames(sheet3_long)
 
 
 #Omitting columns that are not required
@@ -140,40 +89,77 @@ sheet3_long <- sheet3_long %>%
   select(-c(6:17))
 
 
-#check dimensions, column names
-
-colnames(sheet3_long)
-dim(sheet3_long)
-
-
 #Change col names
 
 colnames(sheet3_long) <- c("internal_id", "going_out", "gender", "age", "country", "candy", "rating")
-colnames(sheet3_long)
 
 
 #Add column 'year'
 
 sheet3_long <- sheet3_long %>%
   add_column(year = 2017)
-head(sheet3_long)
 
-
-# X X X X X X X X X X X X X X X X X X - - - - - - - - - - - X X X X X X X X X X X X X X X X X 
+#X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X 
 
 # Combine all sheets 
 
 candy_combined <- bind_rows(sheet1_long, sheet2_long, sheet3_long)
 
 
-#check dimensions, column names
+#Changing column data type
 
-colnames(candy_combined)
-dim(candy_combined)
-head(candy_combined)
+candy_combined$age <- as.integer(candy_combined$age)
+candy_combined$year <- as.integer(candy_combined$year)
+candy_combined$internal_id <- as.integer(candy_combined$internal_id)
 
 
-#Write to csv file - initial iteration of data cleaning
+# remove age greater than 122
 
-write_csv(candy_combined, "raw_data/candy_combined.csv")
+candy_combined <- candy_combined %>%
+  filter(age <= 122)
 
+
+# Change string values to lower case
+
+candy_combined <- candy_combined %>%
+  mutate_if(is.character, str_to_lower)
+
+
+# Cleaning candy column
+
+candy_combined <- candy_combined %>%
+  mutate(candy = str_extract(candy, "[^\\[\\]]+"),
+         candy = str_extract(candy, "[^q6 |]+[^|]+"))
+
+
+#cleaning country column
+
+canada_error <- c("can", "canada`")
+uk_error <- c("endland", "england", "scotland", "u.k.", "united kindom", "united kingdom")
+usa_error <- c("'merica", "america", "merica", "murica", "murrika", "n. america", "new jersey", "new york", "north carolina", "pittsburgh", 
+               "the united states", "the united states of america", "the yoo ess of aaayyyyyy", "trumpistan", "u s", "u s a", "u.s.", "u.s.a.",  
+               "unhinged states", "unied states", "unite states", "united  states of america", "united sates", "united staes", "united state", 
+               "united statea", "united stated", "united states", "united states of america",
+               "united statss", "united stetes", "united ststes", "unites states", "units states", "us", "us of a", "usa usa usa", "usa usa usa usa",
+               "usa usa usa!!!!", "usa!", "usa! usa!", "usa! usa! usa!", "usa!!!!!!", "usa? hard to tell anymore..", "usaa", "usas", "usausausa",   
+               "ussa")
+candy_combined <- candy_combined %>%
+  mutate(country = ifelse(country %in% canada_error, "canada", country),
+         country = ifelse(country %in% uk_error, "uk", country),
+         country = ifelse(country %in% usa_error, "usa", country))
+
+
+# Cleaning candy column
+
+candy_combined <- candy_combined %>%
+  filter(candy != "cash, or other forms of legal tender")
+
+
+
+#saving to new df
+candy_clean <- candy_combined
+
+
+#Write to csv file
+
+write_csv(candy_clean, "../clean_data/candy_clean.csv")
